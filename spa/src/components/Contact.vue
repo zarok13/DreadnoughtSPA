4d0932ae7bdef255bb2f3c4cbe8e49c532b347bc
zarok13 <simon13m@gmail.com>
2020-06-05 10:35:21 +0400

    map testing

--- spa/src/components/Contact.vue
+++ spa/src/components/Contact.vue
@@ -50,7 +50,6 @@
                   </p>
                 </div>
               </div>
-
               <!-- Contact Form -->
               <div class="s-12 m-12 l-6">
                 <h2 class="text-uppercase text-strong margin-bottom-30">Contact Us</h2>
@@ -106,45 +105,33 @@
           </div>
         </div>
       </article>
-<!--      <script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>-->
-<!--      <link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet'/>-->
-<!--      <div class="background-primary padding text-center">-->
-<!--        <a href="/">-->
-<!--          <i class="icon-facebook_circle icon2x text-white"></i>-->
-<!--        </a>-->
-<!--        <a href="/">-->
-<!--          <i class="icon-twitter_circle icon2x text-white"></i>-->
-<!--        </a>-->
-<!--        <a href="/">-->
-<!--          <i class="icon-google_plus_circle icon2x text-white"></i>-->
-<!--        </a>-->
-<!--        <a href="/">-->
-<!--          <i class="icon-instagram_circle icon2x text-white"></i>-->
-<!--        </a>-->
-<!--      </div>-->
       <!-- MAP -->
-      <div class="s-12 grayscale center">
-        <div id="map" class="map" width="100%" height="450">
-          <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
-        </div>
-        
+      <MglMap 
+          :accessToken="accessToken" 
+          :mapStyle="mapStyle"
+          @load="onMapLoaded"
+          :attributionControl="false"
+          >
 
-<!--        <iframe-->
-<!--          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1459734.5702753505!2d16.91089086619977!3d48.577103681657675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssk!2ssk!4v1457640551761"-->
-<!--          width="100%"-->
-<!--          height="450"-->
-<!--          frameborder="0"-->
-<!--          style="border:0"-->
-<!--        ></iframe>-->
-      </div>
+            <MglScaleControl />
+            <!-- <MglPopup :coordinates="markerCoordinates">
+              <span>Hello world!</span>
+            </Mgl -->Popup>
+            <MglMarker :coordinates="markerCoordinates" color="green" >
+              <span slot="marker">mdi-map-marker</span>
+            </MglMarker>
+          </MglMap>
     </main>
-<div style="margin-top: 300px"></div>
+<div   style="margin-top: 278px"></div>
     <Footer />
   </div>
 </template>
 <script>
-import Mapbox from "mapbox-gl";
-import { MglMap } from "vue-mapbox";
+
+// import Mapbox from "mapbox-gl";
+import { MglMap, 
+    MglScaleControl,
+    MglMarker } from "vue-mapbox";
 import Header from "../components/blocks/Header";
 import Footer from "../components/blocks/Footer";
 
@@ -153,16 +140,34 @@ export default {
   components: {
     Header,
     Footer,
-    MglMap
+    MglMap,
+    MglScaleControl,
+    MglMarker
   },
   data() {
     return {
       accessToken: "pk.eyJ1IjoiemFyb2siLCJhIjoiY2s4Njl5eHpxMDA3dTNubGs5ZDh6ZjJyciJ9.9KM0OsYdbSC3KzlF2tjUEw",
-      mapStyle: "mapbox://styles/mapbox/streets-v9"
+      mapStyle: "mapbox://styles/mapbox/streets-v9",
+      markerCoordinates:[
+        44.7, 
+        41.7
+      ]
     };
   },
   created() {
-    this.mapbox = Mapbox;
+    this.map = null;
+  },
+  methods:{
+     async onMapLoaded(event) {
+      const asyncActions = event.component.actions
+
+      const newParams = await asyncActions.flyTo({
+        center: [44.7, 41.7],
+        zoom: 10,
+        speed: 1,
+      })
+      console.log(newParams)
+    }
   }
 };
 </script>
