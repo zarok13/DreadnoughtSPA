eb9fa3c92234164758e4be91e6fce79ecd1fde5d
Simon <erron13m@gmail.com>
2020-11-11 12:00:09 +0400

    ..

--- API/routes/api.php
+++ API/routes/api.php
@@ -11,13 +11,7 @@ Route::group(['namespace' => 'API'], function () {
     Route::get('/configs', 'ConfigsController@index');
     Route::get('/home', 'HomeController@index');
 
-    Route::get('/menu', 'IndexController@menu');
-    Route::get('/slider', 'IndexController@getSlider');
-    Route::get('/intro_section_1', 'IndexController@getIntro1');
-    Route::get('/intro_section_2', 'IndexController@getIntro2');
-    Route::get('/intro_section_3', 'IndexController@getIntro3');
-    Route::get('/blog_list', 'IndexController@blogList');
-    Route::get('/footer', 'IndexController@getFooter');
+    
     Route::get('/mapbox', 'IndexController@mapbox');
     Route::get('/static_content', 'IndexController@staticContent');
 
